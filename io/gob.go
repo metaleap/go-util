6fb58522addae9848223fe56e5e387960754e2f0
metaleap <philipp.schumann@gmail.com>
2012-11-16 23:03:27 +0800

    Totally spontaneous, uncalled-for mini-fix

--- io/gob.go
+++ io/gob.go
@@ -8,30 +8,25 @@ import (
 	util "github.com/metaleap/go-util"
 )
 
-func CreateGobsFile (targetFilePath string, recs *[]interface{}, getRecPtr util.AnyToAny, gzipped bool) {
-	var file, err = os.Create(targetFilePath)
+func CreateGobsFile (targetFilePath string, recs []interface{}, getRecPtr util.AnyToAny, gzipped bool) (err error) {
+	var file *os.File
 	var gobber *gob.Encoder
 	var gzipper *gzip.Writer
-	if file != nil {
+	if file, err = os.Create(targetFilePath); file != nil {
 		defer file.Close()
 	}
-	if err != nil {
-		panic(err)
-	}
+	if err != nil { return }
 	if gzipped {
 		if gzipper, err = gzip.NewWriterLevel(file, gzip.BestCompression); gzipper != nil {
 			defer gzipper.Close()
 			gobber = gob.NewEncoder(gzipper)
 		}
-		if err != nil {
-			panic(err)
-		}
+		if err != nil { return }
 	} else {
 		gobber = gob.NewEncoder(file)
 	}
-	for _, rec := range *recs {
-		if err = gobber.Encode(util.PtrVal(getRecPtr(rec))); err != nil {
-			panic(err)
-		}
+	for _, rec := range recs {
+		if err = gobber.Encode(util.PtrVal(getRecPtr(rec))); err != nil { return }
 	}
+	return
 }
