f2baf36c73c3187b6304949dc21938746e88a673
metaleap <philipp.schumann@gmail.com>
2012-12-08 22:07:24 +0800

    gfx.Rgba32, gfx.Rgba64

--- gfx/gfx.go
+++ gfx/gfx.go
@@ -6,6 +6,18 @@ import (
 	"os"
 )
 
+type Rgba32 struct {
+	R, G, B, A float32
+}
+
+func NewRgba32(vals []float64) *Rgba32 {
+	return &Rgba32{R: float32(vals[0]), G: float32(vals[1]), B: float32(vals[2]), A: float32(vals[3])}
+}
+
+type Rgba64 struct {
+	R, G, B, A float64
+}
+
 //	If 3-dimensions are represented in a 1-dimensional linear array, this function provides one way to return a 1D index addressing a 3D coordinate...
 func Index3D(x, y, z, xsize, ysize int) int {
 	return (((z * xsize) + x) * ysize) + y
