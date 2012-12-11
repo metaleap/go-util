7fde6e67ea806a16b7f90991ee0168644916183f
metaleap <philipp.schumann@gmail.com>
2012-12-11 14:46:22 +0800

    Ifu64

--- util.go
+++ util.go
@@ -93,6 +93,14 @@ func Ifu32(cond bool, ifTrue, ifFalse uint32) uint32 {
 	return ifFalse
 }
 
+//	Returns ifTrue if cond is true, otherwise returns ifFalse.
+func Ifu64(cond bool, ifTrue, ifFalse uint64) uint64 {
+	if cond {
+		return ifTrue
+	}
+	return ifFalse
+}
+
 //	Attempts to extract major and minor version components from a string that begins with a version number.
 //	Example: returns []int{3, 2} and float64(3.2) for a verstr that is "3.2.0 - Build 8.15.10.2761"
 func ParseVersion(verstr string) (majorMinor []int, both float64) {
