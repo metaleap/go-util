a24149e4c841e678da8bffc649bdff8401d3477e
metaleap <philipp.schumann@gmail.com>
2013-02-18 13:41:04 +0700

    str/matcher.go

--- str/matcher.go
+++ str/matcher.go
@@ -8,10 +8,13 @@ type matcherPattern struct {
 	pattern, prefix, suffix, contains string
 }
 
+//	Matches a string against "simple-patterns".
+//	Simple-patterns are strings that can have *-wildcards only at the beginning, at the end, or both.
 type Matcher struct {
 	patterns []*matcherPattern
 }
 
+//	Initializes and returns a new Matcher with the specified patterns.
 func NewMatcher(patterns ...string) (me *Matcher) {
 	me = &Matcher{}
 	for _, s := range patterns {
@@ -20,6 +23,7 @@ func NewMatcher(patterns ...string) (me *Matcher) {
 	return
 }
 
+//	Adds the specified pattern to me.
 func (me *Matcher) AddPattern(s string) {
 	mp := &matcherPattern{pattern: s}
 	if strings.HasPrefix(s, "*") && strings.HasSuffix(s, "*") {
@@ -32,6 +36,7 @@ func (me *Matcher) AddPattern(s string) {
 	me.patterns = append(me.patterns, mp)
 }
 
+//	Returns whether any of the patterns specified for this Matcher contains a *-wildcard.
 func (me *Matcher) HasWildcardPatterns() bool {
 	for _, mp := range me.patterns {
 		if len(mp.contains) > 0 || len(mp.prefix) > 0 || len(mp.suffix) > 0 {
@@ -41,10 +46,13 @@ func (me *Matcher) HasWildcardPatterns() bool {
 	return false
 }
 
+//	Convenience short-hand that informs whether Match(s) returns a non-empty result value.
 func (me *Matcher) IsMatch(s string) bool {
 	return len(me.Match(s)) > 0
 }
 
+//	Matches s against all patterns in me.
+//	Returns the first pattern (minus wildcards) that matches s, or "" if there is no match.
 func (me *Matcher) Match(s string) string {
 	for _, mp := range me.patterns {
 		if len(mp.prefix) > 0 && strings.HasPrefix(s, mp.prefix) {
