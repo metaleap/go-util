146b7662f66e238d0cb7602bb3d289cdf3083b11
metaleap <philipp.schumann@gmail.com>
2013-03-01 12:42:52 +0800

    Optimized ustr.Matcher

--- str/matcher.go
+++ str/matcher.go
@@ -11,7 +11,8 @@ type matcherPattern struct {
 //	Matches a string against "simple-patterns".
 //	Simple-patterns are strings that can have *-wildcards only at the beginning, at the end, or both.
 type Matcher struct {
-	patterns []matcherPattern
+	patterns     []matcherPattern
+	hasWildcards bool
 }
 
 //	Adds the specified pattern to me.
@@ -28,41 +29,35 @@ func (me *Matcher) AddPatterns(patterns ...string) {
 		} else if strings.HasSuffix(s, "*") {
 			patts[i].prefix = s[:len(s)-1]
 		}
+		if len(patts[i].contains) > 0 || len(patts[i].prefix) > 0 || len(patts[i].suffix) > 0 {
+			me.hasWildcards = true
+		}
 	}
 	me.patterns = append(me.patterns, patts...)
 }
 
 //	Returns whether any of the patterns specified for this Matcher contains a *-wildcard.
 func (me *Matcher) HasWildcardPatterns() bool {
-	for i := 0; i < len(me.patterns); i++ {
-		if len(me.patterns[i].contains) > 0 || len(me.patterns[i].prefix) > 0 || len(me.patterns[i].suffix) > 0 {
-			return true
-		}
-	}
-	return false
-}
-
-//	Convenience short-hand that informs whether Match(s) returns a non-empty result value.
-func (me *Matcher) IsMatch(s string) bool {
-	return len(me.Match(s)) > 0
+	return me.hasWildcards
 }
 
 //	Matches s against all patterns in me.
-//	Returns the first pattern (minus wildcards) that matches s, or "" if there is no match.
-func (me *Matcher) Match(s string) string {
+func (me *Matcher) IsMatch(s string) bool {
 	for i := 0; i < len(me.patterns); i++ {
 		if s == me.patterns[i].pattern {
-			return me.patterns[i].pattern
+			return true
 		}
-		if len(me.patterns[i].prefix) > 0 && strings.HasPrefix(s, me.patterns[i].prefix) {
-			return me.patterns[i].prefix
-		}
-		if len(me.patterns[i].suffix) > 0 && strings.HasSuffix(s, me.patterns[i].suffix) {
-			return me.patterns[i].suffix
-		}
-		if len(me.patterns[i].contains) > 0 && strings.Contains(s, me.patterns[i].contains) {
-			return me.patterns[i].contains
+		if me.hasWildcards {
+			if len(me.patterns[i].prefix) > 0 && strings.HasPrefix(s, me.patterns[i].prefix) {
+				return true
+			}
+			if len(me.patterns[i].suffix) > 0 && strings.HasSuffix(s, me.patterns[i].suffix) {
+				return true
+			}
+			if len(me.patterns[i].contains) > 0 && strings.Contains(s, me.patterns[i].contains) {
+				return true
+			}
 		}
 	}
-	return ""
+	return false
 }
