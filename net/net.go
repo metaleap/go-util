979998efefa3f2c6dfedd447105f36da30bd51ad
metaleap <philipp.schumann@gmail.com>
2013-06-14 20:59:41 +0200

    unet.Addr()

--- net/net.go
+++ net/net.go
@@ -2,26 +2,36 @@ package unet
 
 import (
 	"bytes"
+	"fmt"
 	"io"
 	"net/http"
+	"strings"
 
+	ugo "github.com/metaleap/go-util"
 	uio "github.com/metaleap/go-util/io"
 )
 
-//	Implements http.ResponseWriter with a bytes.Buffer
-type ResponseBuffer struct {
-	//	Used to implement http.ResponseWriter.Write()
-	bytes.Buffer
-
-	//	Used to implement http.ResponseWriter.Header()
-	Resp http.Response
+func Addr(protocol, tcpAddr string) (fullAddr string) {
+	localhost := ugo.HostName()
+	both := strings.Split(tcpAddr, ":")
+	if len(both) < 1 {
+		both = []string{localhost}
+	} else if len(both[0]) == 0 {
+		both[0] = localhost
+	}
+	if len(both) > 1 {
+		if both[1] == protocol {
+			both[1] = ""
+		}
+		if len(both[1]) == 0 {
+			both = both[:1]
+		}
+	}
+	if fullAddr = strings.Join(both, ":"); len(protocol) > 0 {
+		fullAddr = fmt.Sprintf("%s://%s", protocol, fullAddr)
+	}
+	return
 }
-
-//	Returns me.Resp.Header
-func (me *ResponseBuffer) Header() http.Header { return me.Resp.Header }
-
-//	No-op
-func (me *ResponseBuffer) WriteHeader(_ int) {}
 
 //	Downloads a remote file to the specified local file path.
 func DownloadFile(fileUrl, filePath string) (err error) {
@@ -41,3 +51,18 @@ func OpenRemoteFile(fileUrl string) (rc io.ReadCloser, err error) {
 	}
 	return
 }
+
+//	Implements http.ResponseWriter with a bytes.Buffer
+type ResponseBuffer struct {
+	//	Used to implement http.ResponseWriter.Write()
+	bytes.Buffer
+
+	//	Used to implement http.ResponseWriter.Header()
+	Resp http.Response
+}
+
+//	Returns me.Resp.Header
+func (me *ResponseBuffer) Header() http.Header { return me.Resp.Header }
+
+//	No-op
+func (me *ResponseBuffer) WriteHeader(_ int) {}
